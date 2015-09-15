0f703a40ead3fb0a4755acc6aab42419f111903f
Freddy Bravo <fbravoxperia@gmail.com>
2015-09-14 22:46:17 -0500

    Modificando
    modificando

---
+++ solucion-quien-es-mejor/QuienEsElMejor/MvcQuienEsElMejor/App_Start/BundleConfig.cs
@@ -0,0 +1,22 @@
+﻿using System.Web;
+using System.Web.Optimization;
+
+namespace MvcQuienEsElMejor
+{
+    public class BundleConfig
+    {
+        // For more information on bundling, visit http://go.microsoft.com/fwlink/?LinkId=301862
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+
+            bundles.Add(new ScriptBundle("~/bundles/script").Include(
+                      "~/Scripts/jquery-2.1.1.min.js",
+                      "~/Scripts/materialize.min.js",
+                      "~/Scripts/main.js"));
+
+            bundles.Add(new StyleBundle("~/Content/css").Include(
+                      "~/Content/ghpages-materialize.css",
+                      "~/Content/main.css"));
+        }
+    }
+}
---
+++ solucion-quien-es-mejor/QuienEsElMejor/MvcQuienEsElMejor/Controllers/contendoresController.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcQuienEsElMejor.Controllers
+{
+    public class contendoresController : Controller
+    {
+        // GET: contendores
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: contendores/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: contendores/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: contendores/Create
+        [HttpPost]
+        public ActionResult Create(FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: contendores/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: contendores/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: contendores/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: contendores/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
